fbf800438e06b96cb0149dd02b2b632a40b37771
James Pierce <james@superbasic.xyz>
2025-01-30 21:49:14 -0600

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,107 @@
+use std::fs::read_dir;
+use std::path::Path;
+
+#[derive(Debug)]
+struct Track {
+    file_name: String,
+    name: String,
+    artist: String,
+    album: String,
+}
+
+fn main() {
+    let mut tracks: Vec<Track> = vec![];
+
+    match read_dir("./music") {
+        Err(why) => panic!("{:?}", why),
+        Ok(artists) => {
+            for artist in artists {
+                match artist {
+                    Err(why) => panic!("{:?}", why),
+                    Ok(artist) => {
+                        let path = artist.path();
+                        if path.is_dir() {
+                            // println!("artist: {:?}", artist.path());
+                            match read_dir(path) {
+                                Err(why) => panic!("{:?}", why),
+                                Ok(albums) => {
+                                    for album in albums {
+                                        match album {
+                                            Err(why) => panic!("{:?}", why),
+                                            Ok(album) => {
+                                                let path = album.path();
+                                                if path.is_dir() {
+                                                    // println!("album: {:?}", album.path());
+                                                    match read_dir(path) {
+                                                        Err(why) => panic!("{:?}", why),
+                                                        Ok(files) => {
+                                                            for file in files {
+                                                                match file {
+                                                                    Err(why) => panic!("{:?}", why),
+                                                                    Ok(file) => {
+                                                                        let file_path = file.path();
+                                                                        let extension =
+                                                                            Path::new(&file_path)
+                                                                                .extension();
+                                                                        // println!(
+                                                                        //     "track: {:?}",
+                                                                        //     &file_path
+                                                                        // );
+                                                                        match extension {
+                                                                            None => {}
+                                                                            Some(extension) => {
+                                                                                match extension
+                                                                                    .to_str()
+                                                                                {
+                                                                                    None => {}
+                                                                                    Some("mp3") => {
+                                                                                        // println!(
+                                                                                        //     "MP3 U BASTERD"
+                                                                                        // );
+                                                                                        let track: Track = Track {
+                                                                                            file_name: file.file_name().into_string().unwrap(),
+                                                                                            name: "track name".to_string(),
+                                                                                            artist: artist.file_name().into_string().unwrap(),
+                                                                                            album: album.file_name().into_string().unwrap(),
+                                                                                        };
+                                                                                        tracks
+                                                                                            .push(
+                                                                                            track,
+                                                                                        );
+                                                                                    }
+                                                                                    Some("jpg") => {
+                                                                                        // println!(
+                                                                                        //     "JPG U BASTERD"
+                                                                                        // );
+                                                                                    }
+                                                                                    Some("png") => {
+                                                                                        // println!(
+                                                                                        //     "PNG U BASTERD"
+                                                                                        // );
+                                                                                    }
+                                                                                    Some(&_) => {}
+                                                                                }
+                                                                            }
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    for track in tracks {
+        println!("{:?}", track)
+    }
+}
