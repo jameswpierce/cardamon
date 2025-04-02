fc17361b16cd00b1b9eb8cee18fa5911b5b5a662
burgerdog <burgerdog@euphoria.institute>
2025-04-02 16:10:39 -0500

    maybe

--- src/cardamon/serve.rs
+++ src/cardamon/serve.rs
@@ -1,11 +1,44 @@
 use crate::cardamon::build;
 use crate::cardamon::config::load_config;
 use axum::Router;
-use notify_debouncer_mini::notify::RecursiveMode;
-use notify_debouncer_mini::{DebounceEventResult, new_debouncer};
+use notify_debouncer_mini::notify::{RecursiveMode};
+use notify_debouncer_mini::{DebounceEventResult, DebouncedEventKind, new_debouncer};
 use std::path::Path;
 use std::time::Duration;
 use tower_http::services::{ServeDir, ServeFile};
+use notify_debouncer_mini::notify::{
+    event::{
+        CreateKind,
+        ModifyKind,
+        RemoveKind,
+        AccessKind,
+    }
+};
+
+fn is_relevant_event(event_kind: &DebouncedEventKind) -> bool {
+    match event_kind {
+        DebouncedEventKind::Create(create_kind) => match create_kind {
+            CreateKind::File => true,      // New file created
+            CreateKind::Folder => true,    // New directory created
+            CreateKind::Any => true,       // Any creation
+            _ => false,
+        },
+        DebouncedEventKind::Modify(modify_kind) => match modify_kind {
+            ModifyKind::Name(_) => true,   // Rename or move
+            ModifyKind::Data(_) => true,   // Content changes
+            ModifyKind::Metadata(_) => false, // Metadata changes (timestamps, permissions)
+            ModifyKind::Any => true,       // Any modification
+            _ => false,
+        },
+        DebouncedEventKind::Remove(remove_kind) => match remove_kind {
+            RemoveKind::File => true,      // File removed
+            RemoveKind::Folder => true,    // Directory removed
+            RemoveKind::Any => true,       // Any removal
+            _ => false,
+        },
+        _ => false,
+    }
+}
 
 #[tokio::main]
 pub async fn serve(dev_mode: bool) -> Result<(), Box<dyn std::error::Error>> {
@@ -15,9 +48,18 @@ pub async fn serve(dev_mode: bool) -> Result<(), Box<dyn std::error::Error>> {
     let mut debouncer = new_debouncer(
         Duration::from_secs(1),
         |res: DebounceEventResult| match res {
-            Ok(_) => {
-                println!("Change detected, triggering rebuild...");
-                let _ = build::build();
+            Ok(events) => {
+                let relevant_events: Vec<_> = events
+                    .into_iter()
+                    .filter(|event| {
+                        is_relevant_event(&event.kind)
+                    })
+                    .collect();
+
+                if !relevant_events.is_empty() {
+                    println!("Change detected, triggering rebuild...");
+                    let _ = build::build();
+                }
             }
             Err(e) => println!("Error {:?}", e),
         },
